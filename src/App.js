bc96cb09be4fbb5dbf309bc7c4bc072b19ab0ea2
Basharat Ali <basharatalidogar14@gmail.com>
2022-01-22 12:18:53 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,320 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef,  useState } from 'react';
+import "./App.css";
+import "./Responsive.css";
+  const App = () => {
+    const [timerDays, setTimerDays] = useState('00');
+    const [timerHours, setTimerHours] = useState('00');
+    const [timerMinutes, setTimerMinutes] = useState('00');
+    const [timerSeconds, setTimerSeconds] = useState('00');
+    
+    let interval = useRef();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const startTimer = () =>{
+        const countdownDate = new Date('May 30, 2022 00:00:00').getTime();
+
+        interval = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = countdownDate - now;
+
+            const days= Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours= Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+            const minutes= Math.floor((distance % (1000 * 60 * 60 )) / (1000 * 60));
+            const seconds= Math.floor((distance % (1000 * 60)) / 1000);
+        
+             if(distance < 0){
+                 //stop out timer
+                 clearInterval(interval.current);
+             } else {
+                 //Update Timer
+                 setTimerDays(days);
+                 setTimerHours(hours);
+                 setTimerMinutes(minutes);
+                 setTimerSeconds(seconds);
+             }
+
+        }, 1000);
+    };
+
+    // component did mount
+    useEffect(()=>{
+        startTimer();
+        return () =>{
+            clearInterval(interval.current);
+        };
+    });
+
+        return (
+            <>
+
+                <section id="header">
+                    <div className="container">
+                        <div className="left">
+                            <img src={process.env.PUBLIC_URL + "/logo-1.webp"} alt="My-Pic" width="100%" />
+                        </div>
+                        <div className="right">
+                            <ul>
+                                <li><a href="">Home</a></li>
+                                <li><a href="">Class</a></li>
+                                <li><a href="">About</a></li>
+                                <li><a href="">Blog</a></li>
+                                <li><a href="">Contact</a></li>
+                                <button>Enroll Now</button>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+
+                <section id="home">
+                    <div className="container" id='box' >
+                        <h1>Quality</h1>
+                        <h1>Learning</h1>
+                        <h2>For Every Child</h2>
+                        <button>Explore Classes</button>
+                    </div>
+                </section>
+
+                <section id="welcome">
+                    <h1>Welcome to our <span>Kindergarten</span></h1>
+                    <p>Our set he for firmament morning sixth subdue today the darkness creeping gathered divide our let god moving today. Moving in fourth air night bring upon lesser subdue fowl male signs.</p>
+                </section>
+
+                <section id="inter">
+                    <div className="container">
+                        <div className="a">
+                            <div className="box">
+                                <div className="img"> <img src={process.env.PUBLIC_URL+ "/about-icon1.svg"} alt="My-Pic" width="130%" /> </div>
+                                  <div className="text">
+                                      <h3>Inter School Sports</h3>
+                                      <p>The words you use in your written communica speak volumes.</p>
+                                  </div>
+                            </div>
+                            <div className="box">
+                                <div className="img"> <img src={process.env.PUBLIC_URL+ "/about-icon2.svg"} alt="My-Pic" width="130%" /> </div>
+                                  <div className="text friendly">
+                                      <h3>Friendly Enviornment</h3>
+                                      <p>The words you use in your written communica speak volumes.</p>
+                                  </div>
+                            </div>
+                            <div className="box">
+                                <div className="img"> <img src={process.env.PUBLIC_URL+ "/about-icon3.svg"} alt="My-Pic" width="130%" /> </div>
+                                  <div className="text">
+                                      <h3>Multiple Classes</h3>
+                                      <p>The words you use in your written communica speak volumes.</p>
+                                  </div>
+                            </div>
+                        </div>
+                        <div className="b">
+                            <img src={process.env.PUBLIC_URL+ "/inter.webp"} alt="My-Pic" width="100%" />
+                        </div>
+                    </div>
+                </section>
+                   
+                <section id="classes">
+                    <div className="container">
+                        <h1>Classes We Offer</h1>
+                        <p>Our set he for firmament morning sixth subdue today the darkness creeping gathered divide our let god moving today. Moving in fourth air night bring upon lesser subdue.</p>
+                       
+                      <div className="box-sec">
+                      <div className="box">
+                           <img src={process.env.PUBLIC_URL+ "/classes-1.webp"} alt="My-Pic" width="100%" />
+                         <div className="text">
+                         <h3>Intelligent Challenge</h3>
+                           <p>The Word you use in your written communi speak volumes.</p>
+                           <hr />
+                           <div className="age">
+                               <div className="a">
+                                   <h3>3-5</h3>
+                                   <p>Age Group</p>
+                               </div>
+                               <div className="b">
+                                   <h3>30</h3>
+                                   <p>Age Group</p>
+                               </div>
+                               <div className="c">
+                                   <h3>10/h</h3>
+                                   <p>Age Group</p>
+                               </div>
+                           </div>
+                         </div>
+                       </div>
+                      <div className="box">
+                           <img src={process.env.PUBLIC_URL+ "/classes-2.webp"} alt="My-Pic" width="100%" />
+                           <div className="text">
+                         <h3>Intelligent Challenge</h3>
+                           <p>The Word you use in your written communi speak volumes.</p>
+                           <hr />
+                           <div className="age">
+                               <div className="a">
+                                   <h3>3-5</h3>
+                                   <p>Age Group</p>
+                               </div>
+                               <div className="b">
+                                   <h3>30</h3>
+                                   <p>Age Group</p>
+                               </div>
+                               <div className="c">
+                                   <h3>10/h</h3>
+                                   <p>Age Group</p>
+                               </div>
+                           </div>
+                         </div>
+                       </div>
+                      <div className="box">
+                           <img src={process.env.PUBLIC_URL+ "/classes-3.webp"} alt="My-Pic" width="100%" />
+                           <div className="text">
+                         <h3>Intelligent Challenge</h3>
+                           <p>The Word you use in your written communi speak volumes.</p>
+                           <hr />
+                           <div className="age">
+                               <div className="a">
+                                   <h3>3-5</h3>
+                                   <p>Age Group</p>
+                               </div>
+                               <div className="b">
+                                   <h3>30</h3>
+                                   <p>Age Group</p>
+                               </div>
+                               <div className="c">
+                                   <h3>10/h</h3>
+                                   <p>Age Group</p>
+                               </div>
+                           </div>
+                         </div>
+                       </div>
+                      </div>
+                      <button>More Classes</button>
+                    </div>
+                </section>
+            
+            <section id="drawing">
+                <div className="box">
+                    <h3>18 Jan 2021</h3>
+                    <h1>Drawing Event</h1>
+                    <p>Our set he for firmament morning sixth subdue today the darkness creeping gathered divide our let god moving today moving in fourth.</p>
+                   <button>View Event</button>
+
+                   <div className="time">
+                   <section>
+                       <h2>{timerDays}</h2>
+                       <p>Days</p>
+                   </section>
+                  
+                   <section>
+                       <h2>{timerHours}</h2>
+                       <p>Hours</p>
+                   </section>
+                   
+                   <section>
+                       <h2>{timerMinutes}</h2>
+                       <p>Minutes</p>
+                   </section>
+                 
+                   <section>
+                       <h2>{timerSeconds}</h2>
+                       <p>Seconds</p>
+                   </section>
+                   </div>
+                </div>
+            </section>
+
+            <section id="expert">
+                <div className="container">
+                    <h1>Expert Teachers</h1>
+                    <p className='para'>Our set he for firmament morning sixth subdue today the darkness creeping gathered divide our let god moving today. Moving in fourth air night bring upon lesser subdue.</p>
+              
+                  <div className="pics">
+                      <div className="box">
+                        <img src={process.env.PUBLIC_URL+"/teacher-1.webp"} alt="My-Pic" width="100%"/>
+                        <h2>Mr.Jackson Clay</h2>
+                        <p>Sports Instructor</p>
+                      </div>
+                      <div className="box">
+                        <img src={process.env.PUBLIC_URL+"/teacher-2.webp"} alt="My-Pic" width="100%"/>
+                        <h2>Aleeza</h2>
+                        <p>Sports Instructor</p>
+                      </div>
+                      <div className="box">
+                        <img src={process.env.PUBLIC_URL+"/teacher-3.webp"} alt="My-Pic" width="100%"/>
+                        <h2>Mr.Jackson Clay</h2>
+                        <p>Sports Instructor</p>
+                      </div>
+                      <div className="box">
+                        <img src={process.env.PUBLIC_URL+"/teacher-2.webp"} alt="My-Pic" width="100%"/>
+                        <h2>Aleeza </h2>
+                        <p>Sports Instructor</p>
+                      </div>
+                  </div>
+                </div>
+            </section>
+
+            <section id="me">
+                <div className="container">
+                    <img src={process.env.PUBLIC_URL+"/comma.webp"} alt="My-Pic" width="6%" />
+                    <p className='para'>"Supercharge your WordPress hosting with detailed website analytics, marketing tools. Our experts are just part of the reason Bluehost is the ideal home for your WordPress website. We're here to help you succeed!</p>
+                   
+                   <div className="king">
+                     <div className="a"> <img src={process.env.PUBLIC_URL+"/me.jpg"} alt="My-Pic" width="30%" /></div>
+                      <div className="b">
+                          <h2>Basharat Ali</h2>
+                          <p>Web Developer</p>
+                      </div>
+                   </div>
+
+                </div>
+            </section>
+
+        <section id="image">
+            <img src={process.env.PUBLIC_URL+"/img-1.webp"} alt="My-Pic" width="25%" />
+            <img src={process.env.PUBLIC_URL+"/img-2.webp"} alt="My-Pic" width="25%" />
+            <img src={process.env.PUBLIC_URL+"/img-3.webp"} alt="My-Pic" width="25%" />
+            <img src={process.env.PUBLIC_URL+"/img-4.webp"} alt="My-Pic" width="25%" />
+        </section>
+
+        <section id="footer">
+            <div className="container">
+                 <div className="foot-sec">
+                <div className="lap">
+                <div className="a">
+                    <img src={process.env.PUBLIC_URL+"/logo-1.webp"} alt="My-Pic" width="40%"/>
+                    <p>Users and submit their own items. You can create different packages and by connecting with your PayPal or Stripe account charge users for registration.</p>
+                   <div className="icons">
+                       <i className='fa fa-twitter'></i>
+                       <i className='fa fa-facebook'></i>
+                       <i className='fa fa-pinterest-p'></i>
+                       <i className='fa fa-linkedin'></i>
+                   </div>
+
+                </div>
+                <div className="b">
+                    <h1>Quick Links</h1>
+                    <ul>
+                        <li><a href="">Classes</a></li>
+                        <li><a href="">Join with us</a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Evnets</a></li>
+                    </ul>
+                </div>
+                </div>
+                <div className="c">
+                    <h1>About Us</h1>
+                    <ul>
+                        <li><a href="">Our Team</a></li>
+                        <li><a href="">Privacy Policy</a></li>
+                        <li><a href="">Testimonial</a></li>
+                        <li><a href="">Programming</a></li>
+                        <li><a href="">User Stories</a></li>
+                    </ul>
+                </div>
+                 </div>
+                <footer>Copyright &copy; basharatalidogar14@gmail.com <i className='fa fa-heart'></i> All Right Reversed!</footer>
+     
+            </div>
+
+          </section>
+
+            </>
+        )
+    }
 
 export default App;
